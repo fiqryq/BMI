8dc9fbd7472d5155977a0796e9fa7fefea0b591c
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-05-13 00:43:19 +0700

    Detail Activity : get Data

--- app/src/main/java/com/suncode/bmi/DetailActivity.kt
+++ app/src/main/java/com/suncode/bmi/DetailActivity.kt
@@ -1,12 +1,23 @@
 package com.suncode.bmi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val intent = Intent(getIntent())
+        datUmur.text = ": " + intent.getStringExtra("UMUR")
+        datBB.text = ": " + intent.getStringExtra("BB") + " kg"
+        datTB.text = ": " + intent.getStringExtra("TB") + " m"
+        datKEL.text = ": " + intent.getStringExtra("JK")
+        datBMI.text = ": " + intent.getStringExtra("BMINUM")
+        datHasil.text = ": " + intent.getStringExtra("BMIRES")
+
     }
 }
--- app/src/main/java/com/suncode/bmi/MainActivity.kt
+++ app/src/main/java/com/suncode/bmi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.suncode.bmi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     // Global Variable
     var spinJenisKelamin = ""
+    var hasilBMI = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,10 +46,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         btHitung.setOnClickListener {
+            val umur = etUmur.text.toString()
             val bb = etBB.text.toString().toFloat()
             val tinggi = etTinggi.text.toString().toFloat() / 100
             val hasil = bb / (tinggi * tinggi)
-            hasilBMI.text = hasil.toString()
+
+            if (hasil < 17) {
+                hasilBMI = "Kurus, Kekurangan berat badan berat"
+            } else if (hasil >= 17 && hasil <= 18.0) {
+                hasilBMI = "Kurus, Kekurangan berat badan ringan"
+            } else if (hasil >= 18.5 && hasil <= 25.0) {
+                hasilBMI = "Normal"
+            } else if (hasil >= 25.1 && hasil <= 27.0) {
+                hasilBMI = "Gemuk, Kelebihan berat badan tingkat ringan"
+            } else if (hasil > 27) {
+                hasilBMI = "Gemuk, Kelebihan berat badan tingkat berat"
+            }
+
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("UMUR", umur)
+            intent.putExtra("BB", bb.toString())
+            intent.putExtra("TB", tinggi.toString())
+            intent.putExtra("JK", spinJenisKelamin)
+            intent.putExtra("BMINUM", hasil.toString())
+            intent.putExtra("BMIRES", hasilBMI)
+            startActivity(intent)
         }
     }
 }
